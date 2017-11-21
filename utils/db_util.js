4e175de3560b277b0ce8e490ece3febb61c3bac9
ekoooo <954408050@qq.com>
2017-11-21 16:15:38 +0800

    数据库工具类

---
+++ utils/db_util.js
@@ -0,0 +1,29 @@
+const { pool } = require('../common/pgsql');
+var Logger = require('../common/logger');
+
+var DBUtil = {
+    /**
+     * 获取序列的下一个值
+     * @param name 如果是 isTableName = true, 则自动拼接成 seq_表名_id
+     * @param isTableName 参数 name 是否为表名
+     */
+    getNextVal: (name, isTableName = true) => new Promise((resolve, reject) => {
+        var seqName = isTableName ? `seq_${name}_id` : name;
+
+        pool.connect().then(client => {
+            client.query(`select nextval($1::text) as seq`, [seqName]).then(rs => {
+                client.release();
+                Logger.info('获取下一个序列：', seqName, rs.rows[0].seq);
+
+                resolve(rs.rows[0].seq);
+            }).catch(e => {
+                client.release();
+                Logger.warn('获取下一个序列：', e.message, e.stack);
+                
+                reject(e.message);
+            });
+        });
+    }),
+};
+
+module.exports = DBUtil;
