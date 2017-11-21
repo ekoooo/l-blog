77e114c3254d4cb5f0e0c914d7a90ddd5b0575e2
ekoooo <954408050@qq.com>
2017-11-21 16:11:57 +0800

    JWT Token 处理类

---
+++ common/token.js
@@ -0,0 +1,29 @@
+/**
+ * JWT Token 处理类
+ */
+var Token = {
+    /**
+     * 生成 JWT Token
+     */
+    jwtEncode: function(payload) {
+
+    },
+
+    /**
+     * 获取 Token 中 payload 信息
+     * @param token
+     * @param isValid 是否验证 Token 的正确性，默认验证
+     */
+    getPayloadInfo: function(token, isValid = true) {
+
+    },
+
+    /**
+     * 验证 JWT Token 的正确性以及有效性
+     */
+    validToken: function(token) {
+
+    },
+};
+
+module.exports = Token;
