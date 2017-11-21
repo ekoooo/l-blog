b9525d2a0a88fac2e1032f8346136c97f5a37771
ekoooo <954408050@qq.com>
2017-11-21 16:11:19 +0800

    postgresql 数据库连接池

---
+++ common/pgsql.js
@@ -0,0 +1,23 @@
+const { Pool } = require('pg');
+const Config = require('../config/');
+var Logger = require('../common/logger');
+
+const pool = new Pool({
+    host: Config.dbHost,
+    user: Config.dbUser,
+    database: Config.dbDatabase,
+    password: Config.dbPassword,
+    port: Config.dbPort,
+    max: Config.dbMax,
+    min: Config.dbMin,
+    idleTimeoutMillis: Config.dbIdleTimeoutMillis,
+    connectionTimeoutMillis: Config.dbConnectionTimeoutMillis,
+});
+
+pool.on('error', (error, client) => {
+    Logger.error('database pool on error', error);
+});
+
+module.exports = {
+    pool,
+};
