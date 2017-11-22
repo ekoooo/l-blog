0d7e1059f962faf7ba19e8222f0f59f181d385cf
ekoooo <954408050@qq.com>
2017-11-22 17:26:38 +0800

    设置可跨域请求，可通过 Header、Method

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,14 +1,24 @@
 var express = require('express');
 var router = express.Router();
 var Logger = require('../../common/logger');
-var OauthLogic = require('../../logic/admin/oauth_logic');
+var OauthLogic = require('../../logic/oauth_logic');
+var Filter = require('../../middlewares/filter');
+
+router.use(function (req, res, next) {
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+    res.header("Content-Type", "application/json;charset=utf-8");
+    
+    next();
+});
 
 // 登录
 router.post('/login', function (req, res, next) {
     var username = req.body.username;
     var password = req.body.password;
 
-    OauthLogic.validAdmin(username, password).then(rs => {
+    OauthLogic.loginAdmin(username, password).then(rs => {
         res.send(rs);
     }).catch(error => {
         res.send(error);
