80036dde27ac228a123e7e98eb9b070a01df4f6e
ekoooo <954408050@qq.com>
2017-11-28 17:06:12 +0800

    update router

--- l-admin/src/router/index.js
+++ l-admin/src/router/index.js
@@ -1,6 +1,7 @@
 import Vue from 'vue';
 import Router from 'vue-router';
 import MSG from '../utils/message';
+import OauthLogic from '../logic/oauth';
 import InfoConfig from '../config/info';
 
 Vue.use(Router);
@@ -10,57 +11,67 @@ const router = new Router({
     routes: [
         {
             path: '/',
-            name: 'index',
-            component: () => import('../pages/index'),
-            meta: {
-                title: '主页',
-                auth: true,
-            }
+            redirect: { name: 'index' },
         },
         {
-            path: '/postCategory',
-            name: 'postCategory',
-            component: () => import('../pages/post_category'),
-            meta: {
-                title: '文章分类',
-                auth: true,
-            }
-        },
-        {
-            path: '/posts',
-            name: 'posts',
-            component: () => import('../pages/posts'),
-            meta: {
-                title: '文章列表',
-                auth: true,
-            }
-        },
-        {
-            path: '/images',
-            name: 'images',
-            component: () => import('../pages/images'),
-            meta: {
-                title: '图片管理',
-                auth: true,
-            }
-        },
-        {
-            path: '/configs',
-            name: 'configs',
-            component: () => import('../pages/configs'),
-            meta: {
-                title: '基础设置',
-                auth: true,
-            }
-        },
-        {
-            path: '/comments',
-            name: 'comments',
-            component: () => import('../pages/comments'),
-            meta: {
-                title: '评论列表',
-                auth: true,
-            }
+            path: '/main',
+            component: () => import('../pages/main'),
+            children: [
+                {
+                    path: 'index',
+                    name: 'index',
+                    component: () => import('../pages/index'),
+                    meta: {
+                        title: '主页',
+                        auth: true,
+                    }
+                },
+                {
+                    path: 'posts',
+                    name: 'posts',
+                    component: () => import('../pages/posts'),
+                    meta: {
+                        title: '文章列表',
+                        auth: true,
+                    }
+                },
+                {
+                    path: 'postCategory',
+                    name: 'postCategory',
+                    component: () => import('../pages/post_category'),
+                    meta: {
+                        title: '文章分类',
+                        auth: true,
+                    }
+                },
+                {
+                    path: 'images',
+                    name: 'images',
+                    component: () => import('../pages/images'),
+                    meta: {
+                        title: '图片管理',
+                        auth: true,
+                    }
+                },
+                {
+                    path: 'configs',
+                    name: 'configs',
+                    component: () => import('../pages/configs'),
+                    meta: {
+                        title: '基础设置',
+                        auth: true,
+                    }
+                },
+                {
+                    path: 'comments',
+                    name: 'comments',
+                    component: () => import('../pages/comments'),
+                    meta: {
+                        title: '评论列表',
+                        auth: true,
+                    }
+                },
+            ],
         },
         {
             path: '/login',
@@ -88,6 +99,12 @@ router.beforeEach((to, from, next) => {
     document.title = `${ InfoConfig.title } - ${ to.meta.title || ''}`;
     
     // 登录权限验证
+    if(to.meta.auth && !OauthLogic.isLogin()) {
+        MSG.error('闯红灯，请登陆！');
+        router.replace({name: 'login'});
+        return;
+    }
+    
     next();
 });
 
