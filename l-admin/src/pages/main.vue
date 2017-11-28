beb57dbccdc98b38fd504793d561c018c235246a
ekoooo <954408050@qq.com>
2017-11-28 17:07:14 +0800

    主页面父级路由页面

---
+++ l-admin/src/pages/main.vue
@@ -0,0 +1,15 @@
+<template>
+    <main-frame>
+        <router-view></router-view>
+    </main-frame>
+</template>
+
+<script>
+    import mainFrame from '../components/base/main-frame';
+
+    export default {
+        components: {
+            mainFrame,
+        }
+    }
+</script>
