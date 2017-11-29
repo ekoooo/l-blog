d863496eaaffc701288401030331d4b0bbc73516
ekoooo <954408050@qq.com>
2017-11-29 11:22:45 +0800

    增加备注

--- config/index.js
+++ config/index.js
@@ -1,6 +1,7 @@
 /**
  * 配置文件主入口，根据开发环境和生产环境引入对应的配置文件
- * 开发环境文件 xxx_dev.js
+ * 开发环境文件 filename.dev.js
+ * 生产环境文件 filename.prod.js （.gitignore 中已忽略，需要自己建立）
  */
 const SUFFIX = process.env.NODE_ENV === 'development' ? '.dev' : '.prod';
 
