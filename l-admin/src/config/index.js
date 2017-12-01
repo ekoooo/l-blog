d632656d865acfa543746ecf8f6f7cd29e97d1d9
ekoooo <954408050@qq.com>
2017-12-01 17:20:13 +0800

    增加七牛云配置

--- l-admin/src/config/index.js
+++ l-admin/src/config/index.js
@@ -1,7 +1,12 @@
 const HOST = 'http://localhost:3000'; // 内部测试
 const API_URL_RPEFIX = HOST + '/';
+const QINIU_UPLOAD_URL = 'http://up-z2.qiniu.com'; // 七牛云上传接口地址
+const QINIU_IMAGE_DOMAIN = 'http://img.lwl.tech/'; // 七牛云配置的域名
+
 
 export {
     HOST,
     API_URL_RPEFIX,
+    QINIU_UPLOAD_URL,
+    QINIU_IMAGE_DOMAIN,
 };
