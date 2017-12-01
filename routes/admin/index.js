5ab07b0809ee429f2fb46c464e73e9ccd3528983
ekoooo <954408050@qq.com>
2017-12-01 17:16:26 +0800

    调整 Header 设置位置

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -3,36 +3,19 @@ let router = express.Router();
 let Logger = require('../../common/logger');
 let OauthLogic = require('../../logic/oauth_logic');
 let Filter = require('../../middlewares/filter');
-
-router.use(function (req, res, next) {
-    res.header("Access-Control-Allow-Origin", "*");
-    res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
-    res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
-    res.header("Content-Type", "application/json;charset=utf-8");
-    
-    next();
-});
+let Misc = require('../../utils/misc');
 
 // 登录
 router.post('/login', function (req, res, next) {
     let username = req.body.username;
     let password = req.body.password;
 
-    OauthLogic.loginAdmin(username, password).then(rs => {
-        res.send(rs);
-    }).catch(error => {
-        res.send(error);
-    });
+    Misc.send(res, OauthLogic.loginAdmin(username, password))
 });
 
 // 退出
-router.post('/logout', Filter.filtHttpLogin, function (req, res, next) {
-    OauthLogic.logout(req.headers.authorization).then(rs => {
-        res.send(rs);
-    }).catch(error => {
-        res.send(error);
-    });
+router.post('/logout', Filter.filtAdminHttpLogin, function (req, res, next) {
+    Misc.send(res, OauthLogic.logout(req.headers.authorization));
 });
-
 
 module.exports = router;
--- routes/index.js
+++ routes/index.js
@@ -1,6 +1,17 @@
-let router = function(app) {
+let express = require('express');
+let router = express.Router();
+
+module.exports = function(app) {
+    app.all('*', function (req, res, next) {
+        res.header("Access-Control-Allow-Origin", "*");
+        res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
+        res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
+        res.header("Content-Type", "application/json;charset=utf-8");
+        
+        next();
+    });
+    
     app.use('/', require('./blog/'));
     app.use('/admin', require('./admin/'));
+    app.use('/admin/qiniu', require('./admin/qiniu'));
 };
-
-module.exports = router;
