b4962ce147cea7284a373b83a73f1a5727b71dcc
ekoooo <954408050@qq.com>
2017-12-01 17:09:45 +0800

    七牛云公共对象

---
+++ common/qiniu.js
@@ -0,0 +1,50 @@
+let qiniu = require('qiniu');
+let http = require('http');
+let Logger = require('./logger');
+let Config = require('../config/');
+
+qiniu.conf.ACCESS_KEY = Config.qiniuAK;
+qiniu.conf.SECRET_KEY = Config.qiniuSK;
+
+let mac = new qiniu.auth.digest.Mac();
+let config = new qiniu.conf.Config();
+    config.zone = qiniu.zone.Zone_z2;
+let bucketManager = new qiniu.rs.BucketManager(mac, config);
+
+/**
+ * 七牛存储 图片处理
+ */
+let Qiniu = {
+    /**
+     * 用于客户端上传
+     * 获取上传 Token
+     */
+    getUploadToken() {
+        let options = {
+            scope: Config.qiniuBucket,
+            expires: 7200, // 自定义凭证有效期单位秒，2小时，默认为一个小时
+        };
+        let putPolicy = new qiniu.rs.PutPolicy(options);
+        
+        return putPolicy.uploadToken(mac);
+    },
+    
+    /**
+     * 获取 AccessToken，用于客户端 API
+     * @param requestURI https://developer.qiniu.com/kodo/api/1731/api-overview
+     * @param reqBody 请求Body，仅当请求的 ContentType 为
+     *                application/x-www-form-urlencoded时才需要传入该参数
+     */
+    getAccessToken(requestURI, reqBody) {
+        return qiniu.util.generateAccessToken(mac, requestURI, reqBody);
+    },
+    
+    /**
+     * 获取 bucketManager 对象
+     */
+    getBucketManager() {
+        return bucketManager;
+    },
+};
+
+module.exports = Qiniu;
