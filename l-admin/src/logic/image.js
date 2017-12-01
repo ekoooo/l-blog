1729194686c4b3ff99cc56d7899851fe3629ff1b
ekoooo <954408050@qq.com>
2017-12-01 17:20:35 +0800

    图片处理相关逻辑

---
+++ l-admin/src/logic/image.js
@@ -0,0 +1,28 @@
+import requester from '../utils/requester';
+
+const Image = {
+    /**
+     * 获取图片列表
+     * @param nextMarker
+     */
+    getList(nextMarker = '') {
+        return requester.get('/admin/qiniu/files?size=20&mark=' + nextMarker);
+    },
+    
+    /**
+     * 获取上传 Token
+     */
+    getUploadToken() {
+        return requester.get('/admin/qiniu/uploadToken');
+    },
+    
+    /**
+     * 删除一张照片
+     * @param key
+     */
+    deleteImage(key) {
+        return requester.delete('/admin/qiniu/' + key);
+    },
+};
+
+export default Image;
