370d7768ea5e86e7befefd4c8584da4eb8c5487e
ekoo <954408050@qq.com>
2017-12-02 23:40:10 +0800

    删除多余引入

--- routes/admin/index.js
+++ routes/admin/index.js
@@ -1,12 +1,11 @@
 let express = require('express');
 let router = express.Router();
-let Logger = require('../../common/logger');
 let OauthLogic = require('../../logic/oauth_logic');
 let Filter = require('../../middlewares/filter');
 let Misc = require('../../utils/misc');
 
 // 登录
-router.post('/login', function (req, res, next) {
+router.post('/login', function (req, res) {
     let username = req.body.username;
     let password = req.body.password;
 
@@ -14,8 +13,8 @@ router.post('/login', function (req, res, next) {
 });
 
 // 退出
-router.post('/logout', Filter.filtAdminHttpLogin, function (req, res, next) {
-    Misc.send(res, OauthLogic.logout(req.headers.authorization));
+router.post('/logout', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, OauthLogic.logout(req.headers['authorization']));
 });
 
 module.exports = router;
