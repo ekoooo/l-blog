45d23e0a98ff39b22e6c141e004ff8a8c1cfc3c4
ekoo <954408050@qq.com>
2017-12-02 23:35:11 +0800

    增加获取总条数和获取分页 SQL 部分

--- utils/db_util.js
+++ utils/db_util.js
@@ -13,17 +13,50 @@ let DBUtil = {
         pool.connect().then(client => {
             client.query(`select nextval($1::text) as seq`, [seqName]).then(rs => {
                 client.release();
-                Logger.info('获取下一个序列：', seqName, rs.rows[0].seq);
+                Logger.info(`getNextVal('${ name }', '${ isTableName }') successed`, seqName, rs.rows[0].seq);
 
                 resolve(rs.rows[0].seq);
             }).catch(e => {
                 client.release();
-                Logger.warn('获取下一个序列：', e.message, e.stack);
+                Logger.warn(`getNextVal('${ name }', '${ isTableName }') error`, e.message, e.stack);
                 
                 reject(e.message);
             });
         });
     }),
+    
+    /**
+     * 获取一张表的记录条数
+     * @param tableName 表名
+     * @param condition 条件 Sql String
+     * @param params 参数
+     */
+    getTotalCount: (tableName, condition = '', params = []) => new Promise((resolve, reject) => {
+        let sql = ` select count(1) as num from ${ tableName } where 1 = 1 ${ condition } `;
+        
+        pool.connect().then(client => {
+            client.query(sql, params).then(rs => {
+                client.release();
+                Logger.info(`getTotalCount('${ tableName }') successed`, rs.rows[0]['num']);
+                
+                resolve(parseInt(rs.rows[0]['num']));
+            }).catch(e => {
+                client.release();
+                Logger.warn(`getTotalCount('${ tableName }') error`, e.message, e.stack);
+            
+                reject(e.message);
+            });
+        });
+    }),
+    
+    /**
+     * 获取分页语句
+     * @param pageId 第几页，从 0 开始数
+     * @param pageSize 一页显示多少条数据
+     */
+    getPagerSqlStr: ({ pageId, pageSize }) => {
+        return ` limit ${pageSize} offset ${ pageId * pageSize } `;
+    }
 };
 
 module.exports = DBUtil;
