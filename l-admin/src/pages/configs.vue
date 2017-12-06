b32127342b6e40639c05846302f32bdb8e2beb25
ekoooo <954408050@qq.com>
2017-12-06 18:34:20 +0800

    测试 markdown-editor 组件

--- l-admin/src/pages/configs.vue
+++ l-admin/src/pages/configs.vue
@@ -1,14 +1,27 @@
 <template>
     <div class="box">
+        <markdown-editor
+            :onchange="onchange"
+        />
     </div>
 </template>
 
 <script>
+    import markdownEditor from '../components/operation/markdown-editor';
+
     export default {
+        methods: {
+            onchange: function (info) {
+                console.log(info);
+            }
+        },
         computed: {
             title: function() {
                 return this.$route.meta.title;
             }
+        },
+        components: {
+            markdownEditor,
         },
     }
 </script>
