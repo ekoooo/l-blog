69f7bbc05bc4fbb17c4e61cc5b488b13ba844945
ekoooo <954408050@qq.com>
2017-12-06 14:31:30 +0800

    提示默认加入取消捕获

--- l-admin/src/utils/message.js
+++ l-admin/src/utils/message.js
@@ -6,7 +6,7 @@ const MSG = {
             confirmButtonText: '确定',
             cancelButtonText: '取消',
             type: 'warning'
-        });
+        }).catch(() => {});
     },
     success: (text) => {
         Message({
