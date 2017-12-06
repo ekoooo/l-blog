133359892f0a7d6a1f9d65b985aee1bf90ee9c3d
ekoooo <954408050@qq.com>
2017-12-06 14:30:19 +0800

    加入编辑分类和删除分类路由

--- routes/admin/post.js
+++ routes/admin/post.js
@@ -5,6 +5,13 @@ let Misc = require('../../utils/misc');
 let PostCategory = require('../../model/admin/post_category');
 
 /**
+ * 获取文章分类
+ */
+router.post('/category/list', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new PostCategory().getPostCategory(req.body));
+});
+
+/**
  * 添加文章分类
  */
 router.post('/category', Filter.filtAdminHttpLogin, function (req, res) {
@@ -12,10 +19,17 @@ router.post('/category', Filter.filtAdminHttpLogin, function (req, res) {
 });
 
 /**
- * 获取文章分类
+ * 添加文章分类
  */
-router.post('/category/list', Filter.filtAdminHttpLogin, function (req, res) {
-    Misc.send(res, new PostCategory().getPostCategory(req.body));
+router.put('/category/:id', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new PostCategory().editPostCategory(req['params']['id'], req.body));
+});
+
+/**
+ * 删除文章
+ */
+router.delete('/category/:id', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new PostCategory().delPostCategoryById(req['params']['id']));
 });
 
 module.exports = router;
