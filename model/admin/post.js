39340cb061df08e0a04a17c8cd51e47f660f86cb
ekoo <954408050@qq.com>
2017-12-09 02:05:24 +0800

    增加“获取文章列表”业务方法

--- model/admin/post.js
+++ model/admin/post.js
@@ -3,6 +3,7 @@ const { pool } = require('../../common/pgsql');
 const CODE = require('../../common/code');
 let Logger = require('../../common/logger');
 let Misc = require('../../utils/misc');
+let DbUtil = require('../../utils/db_util');
 
 class Post {
     constructor() {
@@ -45,7 +46,107 @@ class Post {
             return '简述过长';
         }
         
+        // 便签不能含有“,”号
+        if(formInfo.tags.filter(item => item.indexOf(',') !== -1).length) {
+            return '便签不能含有“,”号';
+        }
+        
         return false;
+    }
+    
+    /**
+     * 获取文章列表
+     * @param formInfo
+     * @returns {Promise<void>}
+     */
+    async getPostList(formInfo) {
+        formInfo.pageId = formInfo.pageId || 0;
+        formInfo.pageSize = formInfo.pageSize || 12;
+    
+        let pagerSql = DbUtil.getPagerSqlStr(formInfo);
+    
+        if(pagerSql === null) {
+            return Promise.reject({
+                code: CODE.ERROR,
+                message: '分页参数错误'
+            })
+        }
+    
+        let sql = `select p.id, p.title, p.key_words, p.up_vote, p.down_vote, p.access_count,
+                       p.comment_check, p.create_time, u.username,
+                       pc.name as category_name, pt.tags
+                   from posts p
+                       left join users u on u.id = p.user_id
+                       left join post_category pc on pc.id = p.post_category_id
+                       left join (select post_id, string_agg(name, ',') as tags from post_tags group by post_id) pt on pt.post_id = p.id
+                   where 1 = 1`;
+        let conditionSql = ` and p.status != -1 `;
+        let orderBySql = ` order by create_time desc `;
+        let params = [];
+    
+        let searchParams = formInfo.q;
+        // 搜索参数
+        if(searchParams) {
+            let index = 0;
+            // title // 标题
+            if(!Misc.isNullStr(searchParams.title)) {
+                sql += ` and p.title like $${ ++index } `;
+                params.push(`%${ searchParams.title }%`);
+            }
+            // categoryName // 分类名
+            if(!Misc.isNullStr(searchParams.categoryName)) {
+                sql += ` and pc.name like $${ ++index } `;
+                params.push(`%${ searchParams.categoryName }%`);
+            }
+            // keyWords // 关键字
+            if(!Misc.isNullStr(searchParams.keyWords)) {
+                sql += ` and p.key_words like $${ ++index } `;
+                params.push(`%${ searchParams.keyWords }%`);
+            }
+            // tag // 标签
+            if(!Misc.isNullStr(searchParams.tag)) {
+                sql += ` and pt.tags like $${ ++index } `;
+                params.push(`%${ searchParams.tag }%`);
+            }
+            // text // 内容
+            if(!Misc.isNullStr(searchParams.text)) {
+                sql += ` and p.plain_text like $${ ++index } `;
+                params.push(`%${ searchParams.text }%`);
+            }
+        }
+        
+        // 分页
+        const dataSql = sql + conditionSql + orderBySql + pagerSql;
+        
+        Logger.info(`get post list form info =>`, formInfo);
+        Logger.info(`get post list sql info =>`, `sql => ${ dataSql }`, `params =>`, params);
+        
+        const client = await pool.connect();
+    
+        try {
+            let rsPromise = client.query(dataSql, params);
+            let countPromise = client.query(`select count(1) as num from (${ sql + conditionSql }) as tmp`, params);
+    
+            let rs = await rsPromise;
+            let count = await countPromise;
+    
+            return Promise.resolve({
+                code: CODE.SUCCESS,
+                list: rs.rows,
+                pageId: formInfo.pageId,
+                pageSize: formInfo.pageSize,
+                totalCount: Number(count.rows[0]['num']),
+            });
+        }catch(e) {
+            Logger.error(`get post list on error => `, e);
+            
+            return Promise.reject({
+                code: CODE.ERROR,
+                message: '添加文章列表失败'
+            });
+        }finally {
+            client.release();
+        }
     }
     
     /**
