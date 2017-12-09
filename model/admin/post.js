47af67f498a8b37d2ea27663e5c11095970a564a
ekoo <954408050@qq.com>
2017-12-09 21:22:00 +0800

    增加 根据ID获取文章，编辑文章 方法

--- model/admin/post.js
+++ model/admin/post.js
@@ -29,7 +29,8 @@ class Post {
             Misc.isNullStr(formInfo.text)) {
             return '内容不能为空';
         }
-        if(Misc.isNullStr(formInfo.desc)) {
+        if(Misc.isNullStr(formInfo.desc) ||
+            Misc.isNullStr(formInfo.descMarkdown)) {
             return '简述不能为空';
         }
         if(Misc.isNullStr(formInfo.keyWords)) {
@@ -42,7 +43,7 @@ class Post {
         if(formInfo.keyWords.length > 128) {
             return '关键字长度不能超过 128 字符';
         }
-        if(formInfo.desc.length > 1024) {
+        if(formInfo.desc.length > 1024 || formInfo.descMarkdown.length > 1024) {
             return '简述过长';
         }
         
@@ -50,8 +51,71 @@ class Post {
         if(formInfo.tags.filter(item => item.indexOf(',') !== -1).length) {
             return '便签不能含有“,”号';
         }
+        // 标签不能有重复相同
+        if(formInfo.tags.length !== new Set(formInfo.tags).size) {
+            return '有重复便签';
+        }
         
         return false;
+    }
+    /**
+     * 根据ID获取一篇文章
+     * @param id
+     * @returns {Promise<void>}
+     */
+    async getPostById(id) {
+        if(Misc.isNullStr(id)) {
+            return Promise.reject({
+                code: CODE.ERROR,
+                message: '参数错误'
+            });
+        }
+        
+        const sql = `select
+                        p.title, p.post_category_id, p.markdown,
+                        p.content_desc_markdown,
+                        p.comment_check::int,
+                        p.key_words, tags
+                    from posts p
+                        left join (select post_id, string_agg(name, ',') as tags from post_tags group by post_id) pt on pt.post_id = p.id
+                    where status <> -1 and id = $1`;
+        
+        const client = await pool.connect();
+        
+        try {
+            let rs = await client.query(sql, [id]);
+            
+            if(rs.rowCount === 0) {
+                return Promise.reject({
+                    code: CODE.ERROR,
+                    message: '文章不存在'
+                });
+            }
+            
+            let info = rs.rows[0];
+            
+            return Promise.resolve({
+                code: CODE.SUCCESS,
+                info: {
+                    title: info['title'],
+                    categoryId: info['post_category_id'],
+                    markdown: info['markdown'],
+                    descMarkdown: info['content_desc_markdown'],
+                    commentCheck: !!info['comment_check'],
+                    keyWords: info['key_words'],
+                    tags: info['tags'].split(','),
+                }
+            });
+        }catch (e) {
+            Logger.error(`get post by id on error => `, e);
+    
+            return Promise.reject({
+                code: CODE.ERROR,
+                message: '获取文章列表失败'
+            });
+        }finally {
+            client.release();
+        }
     }
     
     /**
@@ -152,9 +216,10 @@ class Post {
     /**
      * 添加一个文章
      * @param formInfo
+     * @param id
      * @return {Promise.<void>}
      */
-    async addPost(formInfo) {
+    async addEditPost(formInfo, id) {
         let errMsg = Post.validateAddEditForm(formInfo);
         if(errMsg) {
             return Promise.reject({
@@ -162,50 +227,85 @@ class Post {
                 message: errMsg
             });
         }
-        const postSql = `insert into posts (
-                user_id,
-                post_category_id,
-                title,
-                content,
-                markdown,
-                plain_text,
-                content_desc,
-                key_words,
-                comment_check
-            ) values (
-                $1, $2, $3, $4, $5, $6, $7, $8, $9
-            ) returning id`;
         
-        const postParams = [
-            formInfo['userId'],
-            formInfo['categoryId'],
-            formInfo['title'],
-            formInfo['html'],
-            formInfo['markdown'],
-            formInfo['text'],
-            formInfo['desc'],
-            formInfo['keyWords'],
-            formInfo['commentCheck'] ? 1 : 0,
-        ];
+        const isEdit = !Misc.isNullStr(id);
+        
+        const postSql = isEdit ? `
+                update posts set
+                    post_category_id = $1,
+                    title = $2,
+                    content = $3,
+                    markdown = $4,
+                    plain_text = $5,
+                    content_desc = $6,
+                    content_desc_markdown = $7,
+                    key_words = $8,
+                    comment_check = $9
+                where id = $10
+            ` : `
+                insert into posts (
+                    user_id,
+                    post_category_id,
+                    title,
+                    content,
+                    markdown,
+                    plain_text,
+                    content_desc,
+                    content_desc_markdown,
+                    key_words,
+                    comment_check
+                ) values (
+                    $1, $2, $3, $4, $5, $6, $7, $8, $9, $10
+                ) returning id
+            `;
+        
+        const postParams = isEdit ?
+            [
+                formInfo['categoryId'],
+                formInfo['title'],
+                formInfo['html'],
+                formInfo['markdown'],
+                formInfo['text'],
+                formInfo['desc'],
+                formInfo['descMarkdown'],
+                formInfo['keyWords'],
+                formInfo['commentCheck'] ? 1 : 0,
+                id
+            ] : [
+                formInfo['userId'],
+                formInfo['categoryId'],
+                formInfo['title'],
+                formInfo['html'],
+                formInfo['markdown'],
+                formInfo['text'],
+                formInfo['desc'],
+                formInfo['descMarkdown'],
+                formInfo['keyWords'],
+                formInfo['commentCheck'] ? 1 : 0,
+            ];
         
         const client = await pool.connect();
         
         try {
             await client.query('BEGIN');
+
             // 文章
             let rs = await client.query(postSql, postParams);
-            let postId = rs.rows[0].id;
-            let values = [];
+            let postId = isEdit ? id : rs.rows[0].id;
+    
+            if(isEdit) {
+                // 删除原有标签
+                await client.query(`delete from post_tags where post_id = $1`, [postId]);
+            }
             
+            let values = [];
             formInfo.tags.map(item => {
                 values.push([postId, item]);
             });
-            
-            let tagSql = format('insert into post_tags (post_id, name) values %L', values);
-            await client.query(tagSql);
             // 标签
+            await client.query(format('insert into post_tags (post_id, name) values %L', values));
+
             await client.query('COMMIT');
-            
             return Promise.resolve({
                 code: CODE.SUCCESS,
                 info: postId
@@ -216,7 +316,7 @@ class Post {
             Logger.error(`add post on error => `, e);
             return Promise.reject({
                 code: CODE.ERROR,
-                message: '添加文章失败'
+                message: isEdit ? '编辑文章失败' : '添加文章失败'
             });
         }finally {
             client.release();
