eb0513db1cfa8d3330cc9da68dc2ac8e46f5f4ec
ekoo <954408050@qq.com>
2017-12-10 23:48:01 +0800

    code format

--- routes/admin/qiniu.js
+++ routes/admin/qiniu.js
@@ -4,24 +4,17 @@ let Filter = require('../../middlewares/filter');
 let Misc = require('../../utils/misc');
 let FileManager = require('../../model/admin/file_manager');
 
-/**
- * 获取文件列表
- * /files?size=<size>&mark=[mark]
- */
+// 获取文件列表 => /files?size=<size>&mark=[mark]
 router.get('/files', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new FileManager().getFileList(req.query.size, req.query.mark));
 });
 
-/**
- * 获取上传 Token
- */
+// 获取上传 Token
 router.get('/uploadToken', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new FileManager().getUploadToken());
 });
 
-/**
- * 删除图片
- */
+// 删除图片
 router.delete('/:key', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new FileManager().deleteImage(req['params']['key']));
 });
