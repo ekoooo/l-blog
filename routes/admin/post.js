a9ecbbf1460497aef1f364e972b5d690527698c1
ekoo <954408050@qq.com>
2017-12-10 23:48:13 +0800

    update post router

--- routes/admin/post.js
+++ routes/admin/post.js
@@ -7,14 +7,23 @@ let Post = require('../../model/admin/post');
 let PostTag = require('../../model/admin/post_tag');
 let jwt = require('jwt-simple');
 
-/**
- * 添加文章
- */
+// 获取文章列表
+router.post('/list', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new Post().getPostList(req.body));
+});
+
+// 获取一个文章 => /admin/post/?id=<id>
+router.get('/', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new Post().getPostById(req.query.id));
+});
+
+// 添加文章
 router.post('/', Filter.filtAdminHttpLogin, function (req, res) {
     // 传入用户 ID
     req.body['userId'] = jwt.decode(req.headers['authorization'], null, true, null)['user_id'];
     Misc.send(res, new Post().addEditPost(req.body));
 });
+
 // 编辑文章
 router.put('/:id', Filter.filtAdminHttpLogin, function (req, res) {
     // 传入用户 ID
@@ -22,61 +31,49 @@ router.put('/:id', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new Post().addEditPost(req.body, req['params']['id']));
 });
 
-/**
- * 获取文章列表
- */
-router.post('/list', Filter.filtAdminHttpLogin, function (req, res) {
-    Misc.send(res, new Post().getPostList(req.body));
+// 发布文章
+router.put('/publish/:id', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new Post().updateStatus(req['params']['id'], 1));
 });
 
-/**
- * 获取一个文章
- * /admin/post/?id=<id>
- */
-router.get('/', Filter.filtAdminHttpLogin, function (req, res) {
-    Misc.send(res, new Post().getPostById(req.query.id));
+// 撤回文章
+router.put('/unpublish/:id', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new Post().updateStatus(req['params']['id'], 0));
 });
 
-/**
- * 获取文章分类
- */
+// 删除文章
+router.delete('/:id', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new Post().updateStatus(req['params']['id'], -1));
+});
+
+// 获取文章分类
 router.post('/category/list', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new PostCategory().getPostCategory(req.body));
 });
 
-/**
- * 获取文章分类下拉数据
- */
+// 获取文章分类下拉数据
 router.get('/category', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new PostCategory().getPostCategorySelector());
 });
 
-/**
- * 获取文章标签下拉数据
- */
-router.get('/tag', Filter.filtAdminHttpLogin, function (req, res) {
-    Misc.send(res, new PostTag().getTagSelector());
-});
-
-/**
- * 添加文章分类
- */
+// 添加文章分类
 router.post('/category', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new PostCategory().addPostCategory(req.body));
 });
 
-/**
- * 添加文章分类
- */
+// 编辑文章分类
 router.put('/category/:id', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new PostCategory().editPostCategory(req['params']['id'], req.body));
 });
 
-/**
- * 删除文章分类
- */
+// 删除文章分类
 router.delete('/category/:id', Filter.filtAdminHttpLogin, function (req, res) {
     Misc.send(res, new PostCategory().delPostCategoryById(req['params']['id']));
+});
+
+// 获取文章标签下拉数据
+router.get('/tag', Filter.filtAdminHttpLogin, function (req, res) {
+    Misc.send(res, new PostTag().getTagSelector());
 });
 
 module.exports = router;
