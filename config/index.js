07fd1628fff77075b171a3bd27d8869d47aff18d
ekoooo <954408050@qq.com>
2017-12-14 18:32:02 +0800

    添加网站配置

--- config/index.js
+++ config/index.js
@@ -1,4 +1,5 @@
 /**
+ * 生产环境私有文件需要分开
  * 配置文件主入口，根据开发环境和生产环境引入对应的配置文件
  * 开发环境文件 filename.dev.js
  * 生产环境文件 filename.prod.js （.gitignore 中已忽略，需要自己建立）
@@ -9,4 +10,12 @@ module.exports = {
     ...require('./db' + SUFFIX), // 数据库配置文件
     ...require('./redis' + SUFFIX), // redis 配置文件
     ...require('./qiniu' + SUFFIX), // 七牛存储云配置文件
+    
+    // 网站配置
+    blogInfo: {
+        author: '刘万林,liuwanlin',
+        title: '刘万林的个人网站',
+        description: '刘万林的个人网站',
+        keywords: '刘万林,个人博客,个人网站,web前端,后台开发',
+    },
 };
