79c47c290193146b76666b9b883b40694525c551
ekoooo <954408050@qq.com>
2017-12-14 18:29:49 +0800

    404 处理，添加静态资源目录

--- app.js
+++ app.js
@@ -26,26 +26,10 @@ app.use(cookieParser());
 
 // 静态资源目录
 app.use(express.static(path.join(__dirname, 'public')));
+app.use('/static', express.static(path.join(__dirname, 'l-admin/static')));
+app.use('/manager', express.static(path.join(__dirname, 'l-admin/dist'))); // 后台前端
 
 // 注册路由
 router(app);
-
-// 404 错误处理
-app.use(function(req, res, next) {
-    let err = new Error('Not Found');
-    err.status = 404;
-    next(err);
-});
-
-// error handler
-app.use(function(err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
-});
 
 module.exports = app;
