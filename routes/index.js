1d2b12833036d505d8834d81671cdb792530d8dc
ekoooo <954408050@qq.com>
2017-12-14 18:29:27 +0800

    404 处理

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,11 @@
+let Sender = require('../common/sender');
+
 module.exports = function(app) {
-    app.all('*', function (req, res, next) {
+    // blog
+    app.use('/', require('./blog/index'));
+    
+    // admin header
+    app.all('/admin/*', function (req, res, next) {
         res.header("Access-Control-Allow-Origin", "*");
         res.header("Access-Control-Allow-Headers", "Content-Type, Authorization, X-Requested-With");
         res.header("Access-Control-Allow-Methods", "PUT,POST,GET,DELETE,OPTIONS");
@@ -12,11 +18,23 @@ module.exports = function(app) {
         }
     });
     
-    // blog
-    app.use('/', require('./blog/index'));
-    
-    // admin
+    // admin router
     app.use('/admin', require('./admin/index'));
     app.use('/admin/qiniu', require('./admin/qiniu'));
     app.use('/admin/post', require('./admin/post'));
+    
+    // admin 404
+    app.use('/admin/*', function(req, res) {
+        res.send({
+            code: 404,
+            message: 'Not Found'
+        });
+    });
+    
+    // blog 404
+    app.use(function(req, res) {
+        res.render('404', {
+            ...Sender.mergeBlogInfo()
+        });
+    });
 };
