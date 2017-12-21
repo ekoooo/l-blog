215edeeaced1358282a5e4e4894c52c3d5ae6571
ekoooo <954408050@qq.com>
2017-12-21 15:38:47 +0800

    update column formatter

--- l-admin/src/common/column_formatter.js
+++ l-admin/src/common/column_formatter.js
@@ -1,5 +1,6 @@
+import moment from 'moment';
+
 const ColumnFormatter = {
-    
     /**
      * 文章状态
      */
@@ -12,6 +13,38 @@ const ColumnFormatter = {
         
         return map[cellValue];
     },
+    
+    // 用户性别
+    userGenderFormatter(row, column, cellValue) {
+        const map = {
+            "1": "男",
+            "2": "女",
+        };
+        return map[cellValue];
+    },
+    
+    // 用户状态
+    userStatusFormatter(row, column, cellValue) {
+        const map = {
+            "1": "正常",
+            "0": "停用",
+        };
+        return map[cellValue];
+    },
+    
+    // 投票类型
+    voteTypeFormatter(row, column, cellValue) {
+        const map = {
+            "1": "Like",
+            "-1": "Unlike",
+        };
+        return map[cellValue];
+    },
+    
+    // 时间之类格式化
+    timeFormatter(row, column, cellValue) {
+        return moment(cellValue).format('YYYY-MM-DD HH:mm:ss')
+    }
 };
 
 export default ColumnFormatter;
