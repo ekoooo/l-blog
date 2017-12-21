30fa9753014276df22fb502c34ad11c5515c5fcc
ekoooo <954408050@qq.com>
2017-12-21 15:38:58 +0800

    user logic

---
+++ l-admin/src/logic/users.js
@@ -0,0 +1,22 @@
+import requester from '../utils/requester';
+
+const UsersLogic = {
+    ADMIN_LIST_PAGE_SIZE: 10,
+    ADMIN_LIST_SEARCHPARAMS: {
+        username: undefined, // 用户名
+        nickname: undefined, // 昵称
+    },
+    
+    // 获取管理员列表
+    getAdminList(searchParams) {
+        let defaultParams = {
+            pageId: 0,
+            pageSize: UsersLogic.ADMIN_LIST_PAGE_SIZE
+        };
+    
+        return requester.post('/admin/user?admin=1',
+            Object.assign({}, defaultParams, searchParams));
+    },
+};
+
+export default UsersLogic;
