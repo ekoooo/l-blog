5e34f36d9b471d0b03c610106942d6bdaad79343
ekoooo <954408050@qq.com>
2017-12-21 15:37:18 +0800

    配置显示列组件

---
+++ l-admin/src/components/operation/columns-toggler.vue
@@ -0,0 +1,39 @@
+<template>
+    <div :class="clazz">
+        <el-popover
+            ref="columnspopover"
+            :placement="popoverPopover"
+            width="100"
+            trigger="click">
+            <ul>
+                <li v-for="column in columns">
+                    <el-checkbox
+                        v-model="column.show"
+                        :disabled="column.disabled">
+                        {{ column.label }}
+                    </el-checkbox>
+                </li>
+            </ul>
+        </el-popover>
+        <el-button size="small" v-popover:columnspopover>
+            <i class="fa fa-table"></i>
+            <span>配置显示列</span>
+        </el-button>
+    </div>
+</template>
+
+<script>
+    export default {
+        props: {
+            columns: {
+                type: Object,
+                required: true
+            },
+            popoverPopover: {
+                type: String,
+                "default": 'bottom-start',
+            },
+            clazz: String,
+        }
+    }
+</script>
