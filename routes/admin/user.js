7e84dd91bbe8ec8e1d3079a13823648a0071171e
ekoooo <954408050@qq.com>
2017-12-21 15:35:26 +0800

    add route: user

---
+++ routes/admin/user.js
@@ -0,0 +1,12 @@
+let express = require('express');
+let router = express.Router();
+let Filter = require('../../middlewares/filter');
+let Sender = require('../../common/sender');
+let User = require('../../model/admin/user');
+
+// 获取文章列表
+router.post('/', Filter.filtAdminHttpLogin, function (req, res) {
+    Sender.send(res, new User().getUserList(req.body, req['query']['admin']));
+});
+
+module.exports = router;
