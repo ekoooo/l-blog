c5a465c9a1ed818cefbafb5181c2cfe1a695a16d
ekoooo <954408050@qq.com>
2017-12-21 15:39:05 +0800

    post vote logic

---
+++ l-admin/src/logic/post_vote.js
@@ -0,0 +1,23 @@
+import requester from '../utils/requester';
+
+const PostVote = {
+    PAGE_SIZE: 10,
+    
+    VOTE_LIST_SEARCHPARAMS: {
+        title: undefined, // 文章标题
+        type: undefined, // 投票类型
+    },
+    
+    // 投票列表
+    getVoteList(searchParams) {
+        let defaultParams = {
+            pageId: 0,
+            pageSize: PostVote.PAGE_SIZE
+        };
+    
+        return requester.post('/admin/post/vote/list',
+            Object.assign({}, defaultParams, searchParams));
+    }
+};
+
+export default PostVote;
