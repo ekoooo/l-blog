59d195891db1d630b9df75ee5095c38fba750520
ekoooo <954408050@qq.com>
2017-12-21 15:37:40 +0800

    文章投票类型下拉组件

---
+++ l-admin/src/components/selector/post-vote-type.vue
@@ -0,0 +1,59 @@
+<template>
+    <el-select
+        :placeholder="placeholder"
+        :value="value"
+        :size="size"
+        :disabled="disabled"
+        @input="change($event)">
+        <el-option
+            v-if="emptyOption"
+            :label="emptyOptionLabel"
+            :value="undefined">
+        </el-option>
+        <el-option
+            label="Like"
+            :value="1">
+        </el-option>
+        <el-option
+            label="Unlike"
+            :value="-1">
+        </el-option>
+    </el-select>
+</template>
+
+<script>
+    import PostTagLogic from '../../logic/post_tag';
+
+    export default {
+        props: {
+            value: {
+                'type': Number,
+            },
+            size: {
+                'type': String,
+                'default': 'small'
+            },
+            emptyOption: {
+                'type': Boolean,
+                'default': false
+            },
+            emptyOptionLabel: {
+                'type': String,
+                'default': '全部'
+            },
+            disabled: {
+                'type': Boolean,
+                'default': false
+            },
+            placeholder: {
+                'type': String,
+                'default': 'Like/Unlike'
+            },
+        },
+        methods: {
+            change: function(val) {
+                this.$emit('input', val);
+            },
+        },
+    }
+</script>
