db7a31c096676ac74908c96b73fc15b891eaa61b
ekoooo <954408050@qq.com>
2017-12-21 18:38:23 +0800

    TODO: Token 为什么引入的是一个空对象？~~~~~

--- model/user.js
+++ model/user.js
@@ -1,13 +1,15 @@
 const { pool } = require('../common/pgsql');
 const Misc = require('../utils/misc');
+const CODE = require('../common/code');
 
 let Logger = require('../common/logger');
 let Pwd = require('../common/pwd');
 let redisClient = require('../common/redis');
+// let Token = require('../common/token');
 
 class User {
     constructor() {
-    
+        // this.tokenModel = new Token();
     }
     
     /**
@@ -196,6 +198,73 @@ class User {
     getCacheUserInfo(id) {
         return this.cacheUserinfo(id);
     }
+    
+    /**
+     * 修改用户密码
+     * @param id
+     * @param pwd
+     * @return {Promise.<void>} 返回新 token
+     */
+    // async updatePwd(id, pwd) {
+    //     if(pwd.length < 6 || pwd.length > 16) {
+    //         if(pwd.length < User.PWD_MIN_LEN || pwd.length > User.PWD_MAX_LEN) {
+    //             return Promise.reject({
+    //                 code: CODE.ERROR,
+    //                 message: `密码合法长度应在在${ User.PWD_MIN_LEN }至${ User.PWD_MAX_LEN }位！`,
+    //             });
+    //         }
+    //     }
+    //     if(!Misc.validInt(id, 4)) {
+    //         return Promise.resolve({
+    //             code: CODE.ERROR,
+    //             message: '参数错误',
+    //         });
+    //     }
+    //
+    //     const client = await pool.connect();
+    //
+    //     try {
+    //         // 进行加密
+    //         pwd = await Pwd.hash(pwd);
+    //
+    //         const rs = client.query(`update users set password = $1 where id = $2`, [pwd, id]);
+    //
+    //         if(rs.rowCount === 1) {
+    //             // 删除旧 token
+    //             await this.tokenModel.clearOldCacheToken(id, 0);
+    //
+    //             // 生成新 token
+    //             let userinfoRs = await this.getInfoById(id);
+    //             let token = this.tokenModel.jwtEncode(userinfoRs[0]);
+    //
+    //             // 缓存用户数据
+    //             this.cacheUserinfo(null, userinfoRs[0]).then(() => {
+    //                 Logger.info(`update pwd cache user info to resdis successed =>`, `user id => ${ userinfoRs[0]['user_id'] }`);
+    //             }).catch(error => {
+    //                 Logger.error(`update pwd cache user info to resdis on error =>`, error);
+    //             });
+    //
+    //             return Promise.resolve({
+    //                 code: CODE.SUCCESS,
+    //                 token: token,
+    //             });
+    //         }else {
+    //             return Promise.reject({
+    //                 code: CODE.ERROR,
+    //                 message: '修改密码失败，未改变'
+    //             });
+    //         }
+    //     }catch(e) {
+    //         Logger.error(` user update password on error => `, e);
+    //
+    //         return Promise.reject({
+    //             code: CODE.ERROR,
+    //             message: '修改密码错误'
+    //         });
+    //     }finally {
+    //         client.release();
+    //     }
+    // }
 }
 
 // 初始化一个超级管理员
